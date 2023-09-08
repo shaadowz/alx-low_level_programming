49108fa89265f2d438400022cfe86a63bf107e33
shaadowz <swingsclay@gmail.com>
2023-09-07 17:05:11 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * main - A program to print a line using puts
+ * Return: Always 0 (Success)
+ */
+int main(void)
+puts("and that piece of art is useful\" - Dora Kopar, 2015-10-19");
+return (0);
