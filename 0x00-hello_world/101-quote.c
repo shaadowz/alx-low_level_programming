6d65529e3d0ca9a5a9d21ee0d684f8678475ff2e
shaadowz <swingsclay@gmail.com>
2023-09-07 17:14:20 -0700

    101-q.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - A program to print a line using puts
- * Return: 0 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-puts("and that piece of art is useful\" - Dora Kopar, 2015-10-19");
+fputs("and that piece of art is useful\" - Dora Kopar, 2015-10-19");
 return (0);
 }
