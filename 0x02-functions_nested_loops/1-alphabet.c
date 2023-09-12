b06252a8d50a2a1658c6c74e717307f84a477928
shaadowz <swingsclay@gmail.com>
2023-09-12 13:17:57 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#inclue "main.h"
+
+/**
+ * print_alphabet - Print alphabet in lowercase
+ * Return: 0 
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n);
+
+	return 0;
+}
