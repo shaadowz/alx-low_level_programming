b2953752ba8c6945dc2c092acfff3dbaca26293a
shaadowz <swingsclay@gmail.com>
2023-09-21 12:27:36 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,40 +1,34 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes every word of a string
+ * cap_string - capitalizes everey word of a string
  * @s: string to modify
  *
  * Return: the resulting string
  */
 char *cap_string(char *s)
 {
-    int i;
-    char sep[] = " \t\n,;.!?\"(){}";
+	int i, j;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        int j;
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
 
-        if (s[i] >= 'a' && s[i] <= 'z')
-        {
-            if (i == 0)
-            {
-                s[i] -= 32;
-            }
-            else
-            {
-                for (j = 0; sep[j] != '\0'; j++)
-                {
-                    if (sep[j] == s[i - 1])
-                    {
-                        s[i] -= 32;
-                        break;
-                    }
-                }
-            }
-        }
-    }
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
 
-    return (s);
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == spe[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
+	}
+
+	return (s);
 }
-
