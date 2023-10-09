d478d875211994d394b9c5f34b51761c40b3ee5f
shaadowz <swingsclay@gmail.com>
2023-10-09 06:48:13 -0700

    correction fo task 3

--- 0x0C-more_malloc_free/3-main.c
+++ 0x0C-more_malloc_free/3-main.c
@@ -10,37 +10,38 @@
  *
  * Return: Nothing.
  */
-void simple_print_buffer(char *buffer, unsigned int size)
+void simple_print_buffer(int *buffer, unsigned int size)
 {
-	unsigned int i;
+    unsigned int i;
 
-	i = 0;
-	while (i < size)
-	{
-		if (1 % 10)
-		{
-			printf(" ");
-		}
-		if (!(1 % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", buffer[i]);
-		i++;
-	}
+    i = 0;
+    while (i < size)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
+    }
+    printf("\n");
 }
 
 /**
- * maim - check the code
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int *a;
+    int *a;
 
-	a = array_range(0, 10);
-	simple_print_buffer(a, 11);
-	free(*a);
-	return (0);
+    a = array_range(0, 10);
+    simple_print_buffer(a, 11);
+    free(a);
+    return (0);
 }
